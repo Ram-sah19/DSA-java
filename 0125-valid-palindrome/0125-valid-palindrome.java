1511d685d19092ffd10478e5a03852e2fe3efcd6
Rambilas Sah <ram6070246@gmail.com>
2026-06-29 10:02:13 +0530

    Time: 15 ms (20.65%), Space: 47.3 MB (14.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,12 @@
 class Solution {
-    public boolean isPalindrome(String str) {
-        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        for(int i = 0; i<str.length()/2; i++){
-            int n = str.length();
-            
-            if(str.charAt(i)!= str.charAt(n-1-i)){
-                return false;
-            }
-        
-        }
-        return true;
+    public boolean isPalindrome(String s) {
+
+       
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+       
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        return s.equals(reversed);
     }
 }

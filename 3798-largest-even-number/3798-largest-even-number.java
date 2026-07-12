1e5522c3dc0746df6f6d78530a75af76d846177e
Rambilas Sah <ram6070246@gmail.com>
2026-07-12 15:48:19 +0530

    Time: 1 ms (63.49%), Space: 44.1 MB (24.8%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String largestEven(String s) {
+
+        int i = s.length() - 1;
+
+        while (i >= 0 && (s.charAt(i) - '0') % 2 != 0) {
+            i--;
+        }
+
+        return s.substring(0, i + 1);
+    }
+}

44ae41733126e1d0d264cdff44a7c72dae012c7d
Rambilas Sah <ram6070246@gmail.com>
2026-08-19 22:41:33 +0530

    Time: 142 ms (8.37%), Space: 47.5 MB (33.46%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int min = 0;
+        int max = arr.length;
+
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            // max = Math.max(max, arr[i]);
+        }
+
+        for (int j = min; j <= max; j++) {
+
+            boolean found = false;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (j == arr[i]) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return j;
+            }
+        }
+
+        return -1;
+    }
+}

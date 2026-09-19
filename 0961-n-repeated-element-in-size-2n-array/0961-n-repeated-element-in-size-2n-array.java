a73bb3259243792dec76218e8891a9102442dd07
Rambilas Sah <ram6070246@gmail.com>
2026-09-19 23:12:36 +0530

    Time: 2 ms (48.81%), Space: 47.8 MB (5.69%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -3,7 +3,7 @@ class Solution {
         int count =0;
         for(int i =0; i<nums.length;i++){
             for(int j = nums.length-1; j>=0;j--){
-                if(i !=j && nums[i]==nums[j]){
+                if(i!=j && nums[i]==nums[j]){
                     return nums[i];
                 }
             }
